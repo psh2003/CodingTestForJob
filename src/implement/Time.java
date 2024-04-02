52530c1edc39237177e8ddb4a6ec7b5040c6e5e9
psh2003 <qkrtpgus0312@naver.com>
2024-04-03 00:05:03 +0900

    구현 - 시각(수정)

--- src/implement/Time.java
+++ src/implement/Time.java
@@ -11,21 +11,15 @@ public class Time {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int h=0,m=0,s=0;
         int cnt = 0;
-        while (!("" + h + m + s).equals(n + "5959")) {
-            if (("" + h + m + s).contains("3")) {
-                cnt++;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < 60; j++) {
+                for (int k = 0; k < 60; k++) {
+                    if (("" + i + j + k).contains("3")) {
+                        cnt++;
+                    }
+                }
             }
-            if (s == 60) {
-                s = 0;
-                m++;
-            }
-            if (m == 60) {
-                m = 0;
-                h++;
-            }
-            s++;
         }
         System.out.println(cnt);
     }
